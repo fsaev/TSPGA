f5b269f96ac56dbf7feee4a012df137747872b59
fsaev <fredrik@saevland.com>
2021-08-29 20:54:23 +0200

    New functions for building a graph based on CSV

--- inc/graph.h
+++ inc/graph.h
@@ -3,6 +3,7 @@
 
 typedef struct nvertex nvertex_t;
 typedef struct node node_t;
+typedef struct graph graph_t;
 
 struct nvertex{
     node_t* link;
@@ -17,9 +18,15 @@ struct node{
     int link_count;
 };
 
+struct graph{
+    node_t **nodes;
+    int count;
+};
 
 
 node_t create_node(void *data, int data_len);
 int link_nodes(node_t *from, node_t *to, float distance);
+void create_graph_nodes(graph_t* graph, char **data, int data_len);
+int link_graph_node(graph_t *graph, int index, char **data, int data_len);
 
 #endif
--- src/graph.c
+++ src/graph.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "graph.h"
-
-
 
 node_t create_node(void *data, int data_len)
 {
@@ -16,8 +15,7 @@ node_t create_node(void *data, int data_len)
     return n;
 }
 
-int link_nodes(node_t *from, node_t *to, float distance)
-{
+int link_nodes(node_t *from, node_t *to, float distance) {
         nvertex_t *v = malloc(sizeof(nvertex_t));
         v->link = to; //
         v->distance = distance;
@@ -25,4 +23,24 @@ int link_nodes(node_t *from, node_t *to, float distance)
         from->links[from->link_count++] = v;
         printf("Linked %s to %s with distance %2f\n\r", (char *) from->data, (char *) v->link->data, v->distance);
         return 0;
+}
+
+void create_graph_nodes(graph_t *graph, char **data, int data_len) {
+    graph->nodes = malloc(sizeof(node_t *) * data_len); //Allocate space for nodes
+    graph->count = data_len;
+
+    for(int i = 0; i < data_len; i++){
+        graph->nodes[i] = malloc(sizeof(node_t)); //Allocate node
+        graph->nodes[i]->data = data[i]; //Link content
+        graph->nodes[i]->data_len = strlen(data[i]);
+    }
+}
+
+int link_graph_node(graph_t *graph, int index, char **data, int data_len) {
+    node_t *n = graph->nodes[index];
+
+    for(int i = 0; i < data_len; i++){
+        link_nodes(n, graph->nodes[i], atof(data[i]));
+    }
+    return 0;
 }
