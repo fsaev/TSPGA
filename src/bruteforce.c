2cb70a366c344a4d5881e7ae53d86420bdb817c3
fsaev <fredrik@saevland.com>
2021-08-29 20:51:47 +0200

    Clean up bruteforce.c for new permute

--- src/bruteforce.c
+++ src/bruteforce.c
@@ -1,14 +1,7 @@
-#include <stdlib.h>
 #include "bruteforce.h"
-#include "tsp.h"
-#include "permute.h"
+#include "phenotype.h"
 
 void start_bruteforce()
 {
-    int node_count = get_node_count();
-    int* genome = malloc(sizeof(int) * node_count);
-    for(int i = 0; i < node_count; i++){
-        genome[i] = i;
-    }
-    permute(genome, node_count, measure_fitness);
+    //permute(genome, node_count, measure_fitness);
 }
