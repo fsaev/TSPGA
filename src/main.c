a42ce6cc54a5b2ba97d87c56df05d1a0261433fa
fsaev <fredrik@saevland.com>
2021-08-29 20:55:56 +0200

    Preliminary main function building graph

--- src/main.c
+++ src/main.c
@@ -5,13 +5,18 @@
 #include "tsp.h"
 #include "interface.h"
 
-char *g_entries[30];
+#include "reader_tb.h"
+#include "phenotype.h"
 
 int main(int argc, char *argv[])
 {
-    printf("Hello\n\r");
-    if(argc < 2){ return 1; }
+    char *g_entries[30];
+    graph_t tsp_graph;
+
+
+    //if(argc < 2){ return 1; }
     if(!open_csv(argv[1])){
+    //if(!open_csv("../assets/european_cities.csv")){
         printf("File \"%s\" opened successfully\n\r", argv[1]);
     }else{
         printf("File \"%s\" could not be opened\n\r", argv[1]);
@@ -22,7 +27,7 @@ int main(int argc, char *argv[])
      */
     int len = line_csv(g_entries, 30);
     if(len){
-        create_graph_nodes(g_entries, len);
+        create_graph_nodes(&tsp_graph, g_entries, len);
     }
 
     /*
@@ -31,13 +36,11 @@ int main(int argc, char *argv[])
     int i = 0;
     while((len = line_csv(g_entries, 30))){
         if(len){
-            link_graph_node(i++, g_entries, len);
+            link_graph_node(&tsp_graph, i++, g_entries, len);
         }
     }
 
-    while(1){
-        run_interface();
-    }
-    
+    //start_ga(tsp_graph, 10, 8);
+
     return 0;
 }
